a254ffcbe329375b7d80e8724d8a21102adbc0f9
Saqlain <saqlain.shahzad450@gmail.com>
2026-04-27 20:26:28 +0500

    updated

---
+++ lib/demo.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  List<String> items = ['Flutter', 'Dart', 'React', 'Vue', 'Angular'];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Home Screen'),
+          backgroundColor: Colors.amber,
+        ),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            String item = items[index];
+            return ListTile(
+              title: Text('$index) $item'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailScreen(name: item),
+                  ),
+                );
+              },
+            );
+          },
+          itemCount: items.length,
+        ),
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  final String name;
+  const DetailScreen({super.key, required this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(name), backgroundColor: Colors.amber),
+      body: Center(child: Text(name, style: TextStyle(fontSize: 60))),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:pokemon_api/container_ui.dart';
+import 'package:pokemon_api/demo.dart';
 import 'package:pokemon_api/model.dart';
 
 void main() {
-  runApp(const MyIdea());
+  runApp(HomeScreen());
 }
 
 class MyApp extends StatefulWidget {
